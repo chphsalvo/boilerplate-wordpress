365f9e980bdfbb2729055180e54cf8f8c326a523
Christopher Salvo <chphsalvo@gmail.com>
2021-11-06 11:00:28 +0100

    add DB host

--- src/build.sh
+++ src/build.sh
@@ -17,7 +17,7 @@ echo 'Creating new database...'
 mysql -u $DB_USER "-p$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
 
 echo 'Configuring WP...'
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST
 wp core install --url=$WP_SITE_URL --title=$WP_SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 #copy wp-cli configuration file
 cp ../wp-cli.yml ./
--- src/build_test.sh
+++ src/build_test.sh
@@ -2,11 +2,21 @@
 if [ -f .env ]
 then
   export $(cat .env | sed 's/#.*//g' | xargs -0)
+  echo '.env file loaded...'
 fi
-echo 'build_test'
 
 if [[ "$WP_CUSTOM_THEME" == "yes" ]]
 then
+  echo 'testing DB_...'
+  echo $DB_USER
+  echo $DB_PASSWORD
+  echo $DB_NAME
+  echo $DB_HOST
+fi
+
+if [[ "$WP_CUSTOM_THEME" == "yes" ]]
+then
+  echo 'testing WP_CUSTOM_THEME...'
   echo $WP_CUSTOM_THEME
   echo $WP_CUSTOM_THEME_SLUG
   echo $WP_CUSTOM_THEME_NAME
