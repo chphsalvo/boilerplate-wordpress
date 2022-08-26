8f3ed49595f582059aa72c8ce4fa06eed92860c2
Christopher Salvo <chphsalvo@gmail.com>
2022-08-26 22:01:03 +0200

    fix wp-cli setup folders

--- src/build.sh
+++ src/build.sh
@@ -19,8 +19,6 @@ mysql -u $DB_USER "-p$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
 echo 'Configuring WP...'
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST
 wp core install --url=$WP_SITE_URL --title=$WP_SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
-#copy wp-cli configuration file
-cp ../wp-cli.yml ./
 wp rewrite structure '/%postname%/'
 #create .htaccess file
 wp rewrite flush --hard
